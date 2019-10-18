94ce57f75537f52dadfbf64488313e3b7bb1eb6d
attarepos <46752524+attarepos@users.noreply.github.com>
2019-10-18 19:50:11 +0500

    git repo initailezed

---
+++ renderer.js
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+=======
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+>>>>>>> 6788309ed8dade0f08f3d054190f44ccb45d0db8
